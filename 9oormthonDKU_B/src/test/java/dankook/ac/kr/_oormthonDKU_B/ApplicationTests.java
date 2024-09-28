5b183be4901bf5e2ddcc4e03acaf9a4dbc3d6158
woojuice1102 <122527068+KangWooJu@users.noreply.github.com>
2024-09-29 01:30:06 +0900

    Add files via upload
    feat : 게시판기능 구현 ( 기본적인 CRUD )

---
+++ 9oormthonDKU_B/src/test/java/dankook/ac/kr/_oormthonDKU_B/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dankook.ac.kr._oormthonDKU_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
